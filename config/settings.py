92d0b0bc68f19c96d31a89034deb67e229de0911
MirAnv1545 <mirzalianvarov545@gmail.com>
2025-09-30 17:29:33 +0500

    MaxWay L3Q food index_style ready

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
+STATICFILES_DIRS = [BASE_DIR.joinpath("static")]
 
 
 MEDIA_ROOT = BASE_DIR.joinpath("media")
